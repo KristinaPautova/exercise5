4b2e9ba7a56a132399182b20677f40f8245faf61
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-11-19 12:15:50 +0100

    Проверено

--- index7.js
+++ index7.js
@@ -1,10 +1,10 @@
-var listOfNumbers = [2,3,4,5,6,3,3,0,null,'dscd'];
+var listOfNumbers = [2,3,4,5,6,3,3,0,null,'dscd',NaN];
 var evenNumber=0;
 var notEvenNumber=0;
 var zero = 0;
 for (var i=0;i<listOfNumbers.length;i++){
 
-    if (typeof listOfNumbers[i] !== 'number'){
+    if (typeof listOfNumbers[i] !== 'number' || isNaN(listOfNumbers[i])){
         break;
     }
    else if (listOfNumbers[i] === 0){
@@ -23,3 +23,5 @@ for (var i=0;i<listOfNumbers.length;i++){
 console.log(`четных ${evenNumber}`);
 console.log(`не четных ${notEvenNumber}`);
 console.log(`ноль  ${zero}`);
+
+// Почти правильно, но не учтено значение NaN. Оно тоже имеет тип number, поэтому пройдет проверку на typeof, но при этом NaN не является числом, поэтому его нельзя учитывать при подсчете четных\нечетных и нулей. Добавила выше проверку на NaN.
